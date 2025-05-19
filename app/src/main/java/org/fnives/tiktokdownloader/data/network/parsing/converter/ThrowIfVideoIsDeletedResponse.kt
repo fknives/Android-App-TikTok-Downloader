9a21cd3ad78eccc77cc17ec7741e6a1b1f5165e3
Gergely Hegedus <hegedusgergely@yahoo.com>
2025-05-19 20:45:34 +0300

    Add another option a deleted video can show up

--- app/src/main/java/org/fnives/tiktokdownloader/data/network/parsing/converter/ThrowIfVideoIsDeletedResponse.kt
+++ app/src/main/java/org/fnives/tiktokdownloader/data/network/parsing/converter/ThrowIfVideoIsDeletedResponse.kt
@@ -8,6 +8,8 @@ class ThrowIfVideoIsDeletedResponse {
     fun invoke(html: String) {
         if (html.contains("\"statusMsg\":\"status_deleted")) {
             throw VideoDeletedException(html = html)
+        } else if (html.contains("\"statusMsg\":\"item doesn't exist")) {
+            throw VideoDeletedException(html = html)
         }
     }
 }
